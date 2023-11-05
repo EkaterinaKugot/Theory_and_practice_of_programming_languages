74f8fdf86bf77cb7a8650c30cdc26584ae29422a
EkaterinaKugot <103882214+EkaterinaKugot@users.noreply.github.com>
2023-11-05 18:25:18 +0800

    Add files via upload

---
+++ pascal/interpreter/lexer.py
@@ -0,0 +1,104 @@
+from .token import Token, TokenType
+
+class Lexer():
+
+    def __init__(self):
+        self._pos = 0
+        self._text = ""
+        self._current_char = None
+
+    def init(self, text):
+        self._text = text
+        self._pos = 0
+        self._current_char = self._text[self._pos]
+
+    def forward(self): #переход на след символ
+        self._pos += 1
+        if self._pos > len(self._text) - 1:
+            self._current_char = None
+        else:
+            self._current_char = self._text[self._pos]
+
+    def skip(self): #пропуск пробела
+        while (self._current_char is not None and 
+               self._current_char.isspace()):
+            self.forward()
+
+    def number(self): #определение числа
+        result  = []
+        while (self._current_char is not None and 
+               (self._current_char.isdigit() or
+                self._current_char == ".")):
+            result.append(self._current_char)
+            self.forward()
+        return "".join(result)
+    
+    def is_assignment(self):
+        self.forward()
+        if self._current_char.isspace():
+            self.skip()
+        if self._current_char == '=':
+            self.forward()
+            return True
+        return False
+    
+    def variable(self):
+        result  = []
+        correct_var = False
+        while (self._current_char is not None and 
+               (self._current_char.isalpha() or
+                self._current_char == "_" or
+                self._current_char.isdigit())):
+            if self._current_char.isalpha() and not(correct_var):
+                correct_var = True
+            result.append(self._current_char)
+            self.forward()
+        return "".join(result), correct_var
+
+    def next(self): 
+        while self._current_char:
+            if self._current_char.isspace():
+                self.skip()
+                continue
+            elif self._current_char.isdigit():
+                num = self.number()
+                if self._current_char is None or \
+                    (self._current_char != '_' and not(self._current_char.isalpha())):
+                    return Token(TokenType.NUMBER, num)
+            elif self._current_char in ["+", "-", "/", "*"]:
+                op = self._current_char
+                self.forward()
+                return Token(TokenType.OPERATOR, op)
+            elif self._current_char == "(":
+                op = self._current_char
+                self.forward()
+                return Token(TokenType.LPAREN, op)
+            elif self._current_char == ")":
+                op = self._current_char
+                self.forward()
+                return Token(TokenType.RPAREN, op)
+            elif self._current_char == ":":
+                if self.is_assignment():
+                    return Token(TokenType.ASSIGN, ":=")
+            elif self._current_char == ";":
+                semi = self._current_char
+                self.forward()
+                return Token(TokenType.SEMICOLON, semi)
+            elif self._current_char == ".":
+                return Token(TokenType.DOT, self._current_char)
+            elif self._current_char == "B": #BEGIN
+                begin = ['B', 'E', 'G', 'I', 'N']
+                while self._current_char and self._current_char in begin:
+                    self.forward()
+                return Token(TokenType.BEGIN, "BEGIN")
+            elif self._current_char == "E": #END
+                begin = ['E', 'N', 'D']
+                while self._current_char and self._current_char in begin:
+                    self.forward()
+                return Token(TokenType.END, "END")
+            elif self._current_char.isalpha() or self._current_char == '_':
+                var, correct_var = self.variable()
+                if correct_var:
+                    return Token(TokenType.ID, var)
+            
+            raise SyntaxError("bad token")
---
+++ pascal/tests/test_interpreter.py
@@ -0,0 +1,78 @@
+import pytest
+from interpreter import Interpreter, NodeVisitor, Token, TokenType, Parser
+from interpreter import Number, BinOp, UnOp, Variable, Assignment, Empty, Semicolon
+
+
+@pytest.fixture(scope="function")
+def interpreter():
+    return Interpreter()
+
+class TestInterpreter:
+    interpreter = Interpreter()
+
+    def test_first_program(self, interpreter):
+        assert interpreter.eval("BEGIN END.") == {}
+
+    def test_second_program(self, interpreter):
+        assert interpreter.eval("BEGIN x:= 2 + 3 * (2 + 3); y:= 2 / 2 - 2 + 3 * ((1 + 1) + (1 + 1)); END.") == {'x': 17.0, 'y': 11.0}
+
+    def test_threed_program(self, interpreter):
+        assert interpreter.eval(
+            "BEGIN y: = 2; BEGIN a := 3; a := a; b := -10 ++ a + 10 * y / 4; c := a - b END; x := 11; END."
+            ) == {'y': 2.0, 'a': 3.0, 'b': -2.0, 'c': 5.0, 'x': 11.0}
+        
+    def test_invalid_token_order(self, interpreter):
+        with pytest.raises(SyntaxError):
+            interpreter.eval("x")
+
+    def test_bad_token(self, interpreter):
+        with pytest.raises(SyntaxError):
+            interpreter.eval("BEGIN a : 3")
+
+    def test_invalid_factor(self):
+        with pytest.raises(SyntaxError):
+            Parser().factor()
+    
+    def test_invalid_statement(self):
+        with pytest.raises(SyntaxError):
+            Parser().statement()
+
+    def test_uninitialized_variable(self, interpreter):
+        with pytest.raises(ValueError):
+            interpreter.eval("BEGIN a := b END.")
+    
+    def test_invalid_unary_operator(self, interpreter):
+        with pytest.raises(ValueError):
+            interpreter.visit_unop(UnOp(Token(TokenType.OPERATOR, "^"), Number(Token(TokenType.NUMBER, 2))))
+
+    def test_invalid_operator(self, interpreter):
+        with pytest.raises(ValueError):
+            interpreter.visit_binop(BinOp(Number(Token(TokenType.NUMBER, 2)), Token(TokenType.OPERATOR, "^"), Number(Token(TokenType.NUMBER, 3))))
+
+    def test_number_str(self):
+        assert Number(Token(TokenType.NUMBER, "2")).__str__() == f"Number (Token(TokenType.NUMBER, 2))"
+
+    def test_binop_str(self):
+        assert (BinOp(Number(Token(TokenType.NUMBER, 2)), Token(TokenType.OPERATOR, "+"), Number(Token(TokenType.NUMBER, 3))).__str__() == 
+                f"BinOp+ (Number (Token(TokenType.NUMBER, 2)), Number (Token(TokenType.NUMBER, 3)))")
+        
+    def test_unop_str(self):
+        assert UnOp(Token(TokenType.OPERATOR, "-"), Number(Token(TokenType.NUMBER, 3))).__str__() == f"UnOp (-Number (Token(TokenType.NUMBER, 3)))"
+
+    def test_variable_str(self):
+        assert Variable(Token(TokenType.ID, "x")).__str__() == f"Var (Token(TokenType.ID, x))"
+
+    def test_assignment_str(self):
+        assert Assignment(Variable(Token(TokenType.ID, "x")), Token(TokenType.NUMBER, "3")).__str__() == "Assignment (Var (Token(TokenType.ID, x)) = Token(TokenType.NUMBER, 3))"
+    
+    def test_empty_str(self):
+        assert Empty().__str__() == "Empty ()"
+
+    def test_semicolon_str(self):
+        assert Semicolon(Token(TokenType.NUMBER, "3"), Token(TokenType.NUMBER, "4")).__str__() == "Semicolon (Token(TokenType.NUMBER, 3), Token(TokenType.NUMBER, 4))"
+
+    def test_nodevisitor(self):
+        assert NodeVisitor().visit() == None
+
+
+    
